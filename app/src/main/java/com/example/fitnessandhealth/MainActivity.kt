971f572da15e0ad01927e0ae2d517a4d482a29ce
Christopher Cortes <christopherctes@hotmail.com>
2024-12-10 22:22:55 -0300

    icon image, images, and button

--- app/src/main/java/com/example/fitnessandhealth/MainActivity.kt
+++ app/src/main/java/com/example/fitnessandhealth/MainActivity.kt
@@ -6,42 +6,96 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.ui.unit.dp
 import com.example.fitnessandhealth.ui.theme.FitnessAndHealthTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             FitnessAndHealthTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HealthFitnessApp()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HealthFitnessApp() {
+    MessageText(message = "\t\t\t\tWelcome!\n\nFITNESS & HEALTH")
+    MainMenu()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FitnessAndHealthTheme {
-        Greeting("Android")
+fun MessageText(message: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(
+            modifier = Modifier.height(100.dp)
+        )
+        Text(
+            modifier = Modifier,
+            text = message
+        )
+    }
+}
+
+@Composable
+fun MainMenu(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column (
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.foodw),
+                contentDescription = "food"
+            )
+            Spacer(
+                modifier = Modifier.height(16.dp)
+            )
+            Button(onClick = {/*TODO*/}) {
+                Text(stringResource(R.string.food))
+            }
+        }
+        Column (
+            modifier = Modifier,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.healthyfitnessw),
+                contentDescription = "fitness"
+            )
+            Spacer(
+                modifier = Modifier.height(16.dp)
+            )
+            Button(onClick = {/*TODO*/}) {
+                Text(stringResource(R.string.fitness))
+            }
+        }
     }
 }
